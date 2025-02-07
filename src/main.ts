32bc66ec4647ecda62429988d8158390e5bede8f
CristhianLaid <cmurillos@unemi.edu.ec>
2025-02-07 10:35:13 -0500

    add: implement cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envConfig } from './config/env/configuration.config';
 import { Logger } from '@nestjs/common';
+import { CorsConfigService } from './config/cors/cors.config.service';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.setGlobalPrefix('api')
+
+  // Obtiene la configuración de CORS desde el servicio
+  const corsConfigService = app.get(CorsConfigService);
+  const corsOptions = corsConfigService.getCorsConfigOption();
+  app.enableCors(corsOptions);
+  
   await app.listen(envConfig.PORT ?? 3005);
   logger.log(`Listening in http://localhost:${envConfig.PORT}`);
 }
